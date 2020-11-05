6546c56f10a24cda16aac74bf3371c864b5465d2
ARTHARA <the.first678@gmail.com>
2020-11-05 22:55:15 +0700

    todolist

---
+++ app/src/main/java/com/artharatask/todolist_ads/MainActivity.java
@@ -0,0 +1,14 @@
+package com.artharatask.todolist_ads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
